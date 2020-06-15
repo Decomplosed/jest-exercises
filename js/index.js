bf5ba0334d4bccb00225bb9134b677ab6a1c9492
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-15 23:13:13 +0200

    formatting fixes

--- js/index.js
+++ js/index.js
@@ -3,70 +3,70 @@ import {
   validateName,
   validatePassword,
   validateConfirmPassword,
-  validateEmail
-} from './validators';
-import { setValid, setInvalid } from './utilityFunctions';
-import '../styles/index.css';
+  validateEmail,
+} from './validators'
+import { setValid, setInvalid } from './utilityFunctions'
+import '../styles/index.css'
 
 // Form
-const form = document.getElementById('myForm');
+const form = document.getElementById('myForm')
 
 // Add onfocusout event function
 const addFocusOutEvent = (field) => {
   field.addEventListener('focusout', () => {
     // If field value is invalid, set input to invalid else set valid
-    validateFieldAndSetError(field);
-  });
-};
+    validateFieldAndSetError(field)
+  })
+}
 
 const validateFieldAndSetError = (field) => {
-  const result = validateField(field.id);
+  const result = validateField(field.id)
   if (!result.valid) {
-    setInvalid(field, result.error);
-    return false;
+    setInvalid(field, result.error)
+    return false
   } else {
-    setValid(field);
-    return true;
+    setValid(field)
+    return true
   }
-};
+}
 
 // Get fields and add onfocusout event to them
-const firstNameField = document.getElementById('firstName');
-addFocusOutEvent(firstNameField);
-const lastNameField = document.getElementById('lastName');
-addFocusOutEvent(lastNameField);
-const passwordField = document.getElementById('password');
-addFocusOutEvent(passwordField);
-const confirmPasswordField = document.getElementById('confirmPassword');
-addFocusOutEvent(confirmPasswordField);
-const emailField = document.getElementById('email');
-addFocusOutEvent(emailField);
+const firstNameField = document.getElementById('firstName')
+addFocusOutEvent(firstNameField)
+const lastNameField = document.getElementById('lastName')
+addFocusOutEvent(lastNameField)
+const passwordField = document.getElementById('password')
+addFocusOutEvent(passwordField)
+const confirmPasswordField = document.getElementById('confirmPassword')
+addFocusOutEvent(confirmPasswordField)
+const emailField = document.getElementById('email')
+addFocusOutEvent(emailField)
 
 // Validate field depending on field name
 const validateField = (field) => {
   switch (field) {
     case 'firstName':
-      return validateName(firstNameField.value);
+      return validateName(firstNameField.value)
     case 'lastName':
-      return validateName(lastNameField.value);
+      return validateName(lastNameField.value)
     case 'password':
-      return validatePassword(passwordField.value);
+      return validatePassword(passwordField.value)
     case 'confirmPassword':
       return validateConfirmPassword(
         passwordField.value,
         confirmPasswordField.value
-      );
+      )
     case 'email':
-      return validateEmail(emailField.value);
+      return validateEmail(emailField.value)
     default:
-      return false;
+      return false
   }
-};
+}
 
 // Handle form
 form.addEventListener('submit', (event) => {
   // Prevent default behaviour
-  event.preventDefault();
+  event.preventDefault()
   if (
     validateFieldAndSetError(firstNameField) &&
     validateFieldAndSetError(lastNameField) &&
@@ -74,35 +74,35 @@ form.addEventListener('submit', (event) => {
     validateFieldAndSetError(confirmPasswordField) &&
     validateFieldAndSetError(emailField)
   ) {
-    console.log(validateField('firstName'));
+    console.log(validateField('firstName'))
     // Get user's name
-    const name = firstName.value;
+    const name = firstName.value
     // Get container
-    const container = document.querySelector('div.container');
+    const container = document.querySelector('div.container')
     // Create loading bar
-    const loader = document.createElement('div');
-    loader.className = 'progress';
-    const loadingBar = document.createElement('div');
-    loadingBar.className = 'indeterminate';
-    loader.appendChild(loadingBar);
+    const loader = document.createElement('div')
+    loader.className = 'progress'
+    const loadingBar = document.createElement('div')
+    loadingBar.className = 'indeterminate'
+    loader.appendChild(loadingBar)
     // Add loading bar to container
-    container.appendChild(loader);
+    container.appendChild(loader)
     // Mimicking real world async code
-    setTimeout(function() {
+    setTimeout(function () {
       // Get loading bar
-      const loaderDiv = document.querySelector('div.progress');
-      const panel = document.createElement('div');
-      panel.className = 'card-panel green';
-      const text = document.createElement('span');
-      text.className = 'white-text';
+      const loaderDiv = document.querySelector('div.progress')
+      const panel = document.createElement('div')
+      panel.className = 'card-panel green'
+      const text = document.createElement('span')
+      text.className = 'white-text'
       text.appendChild(
         document.createTextNode(
           `Sign up successful, welcom to SocialApe ${name}`
         )
-      );
-      panel.appendChild(text);
+      )
+      panel.appendChild(text)
       // Replace loading bar with success message
-      container.replaceChild(panel, loaderDiv);
-    }, 1000);
+      container.replaceChild(panel, loaderDiv)
+    }, 1000)
   }
-});
+})
