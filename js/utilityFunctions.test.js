f6b4e405ee7c52e9385c3782e823d5bedd340bd2
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-15 23:25:10 +0200

    added check of isEmpty with whitespace

--- js/utilityFunctions.test.js
+++ js/utilityFunctions.test.js
@@ -3,4 +3,7 @@ import { isEmpty, checkIfEmpty, setValidationRes } from './utilityFunctions'
 test('isEmpty()', () => {
   let result = isEmpty('')
   expect(result).toBeTruthy()
+
+  result = isEmpty(' ')
+  expect(result).toBeFalsy()
 })
