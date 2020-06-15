1dabb3bdeb3617cd95a2e4ac7fa1fea4596b0bf7
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-15 23:29:24 +0200

    test for checkIfEmpty function with whitespaces

--- js/utilityFunctions.test.js
+++ js/utilityFunctions.test.js
@@ -9,3 +9,10 @@ test('isEmpty()', () => {
   result = isEmpty(' ')
   expect(result).toBeFalsy()
 })
+
+test('checkIfEmpty()', () => {
+  result = checkIfEmpty(' ')
+
+  let expectedResult = { valid: false, error: 'Must not be empty' }
+  expect(result).toEqual(expectedResult)
+})
