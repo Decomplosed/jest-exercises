9168b2dc3cc4004a2e2e5e9ebb77a1c9896ba0fa
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-16 13:52:33 +0200

    moved app to the global scope

--- js/index.test.js
+++ js/index.test.js
@@ -1,12 +1,12 @@
 import puppeteer from 'puppeteer'
 
 let browser
+const app =
+  'file:///Users/Bart/Desktop/Courses/classed/jest-exercises/index.html'
 
 test('Validating first name field', async () => {
   browser = await puppeteer.launch()
   const page = await browser.newPage()
-  const app =
-    'file:///Users/Bart/Desktop/Courses/classed/jest-exercises/index.html'
   await page.goto(app)
 
   await page.click('input#firstName')
