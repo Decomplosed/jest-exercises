b48accb4f45e04a12cdf971bc927b5f4a5b2485a
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-16 13:08:57 +0200

    get link to the main file

--- js/index.test.js
+++ js/index.test.js
@@ -3,4 +3,6 @@ import puppeteer from 'puppeteer'
 test('Validating first name field', async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  const app =
+    'file:///Users/Bart/Desktop/Courses/classed/jest-exercises/index.html'
 })
