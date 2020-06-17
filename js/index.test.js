fdff618adfce72ee171fad8a44c581069dfb73d9
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-17 13:00:34 +0200

    started writing test with invalid data

--- js/index.test.js
+++ js/index.test.js
@@ -62,6 +62,36 @@ test('Validating all fields', async () => {
   await browser.close()
 }, 10000)
 
+test('Validating all fields with invalid data', async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 35,
+  })
+  const page = await browser.newPage()
+  await page.goto(app)
+
+  await page.click('input#firstName')
+  await page.type('input#firstName', 'John')
+  await page.click('input#lastName')
+  await page.type('input#lastName', 'Doe')
+  await page.click('input#password')
+  await page.type('input#password', '123456a')
+  await page.click('input#confirmPassword')
+  await page.type('input#confirmPassword', '123456a')
+  await page.click('input#email')
+  await page.type('input#email', 'johndoe@email.com')
+  await page.click('input#firstName')
+
+  try {
+    const invalidInput = await page.$eval('input.invalid', (input) => input)
+    expect(input).toBeUndefined()
+  } catch (err) {
+    expect(err).toBeDefined()
+  }
+
+  await browser.close()
+}, 10000)
+
 test('Fetching success panel', async () => {
   browser = await puppeteer.launch({
     headless: false,
